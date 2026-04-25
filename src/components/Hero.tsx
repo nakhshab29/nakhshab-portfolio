0b7182f7a88807173127e8118ecd84e9ad7ce177
Nakhshab Ansari <ansarinakhshab52@gmail.com>
2026-04-25 21:10:53 +0530

    Mobile UI: Implement vertical tall photo (row-span-2) next to stats

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { metrics, skills, stackItems } from "../data/constants";
 function PhotoCard() {
   return (
     <RevealCard
-      className="col-span-1 md:col-span-4 lg:col-span-2 card-photo relative overflow-hidden rounded-[20px] aspect-[4/5] md:aspect-auto"
+      className="col-span-1 row-span-2 md:col-span-4 lg:col-span-2 card-photo relative overflow-hidden rounded-[20px] md:aspect-auto"
       delay={0.05}
     >
       <motion.img
@@ -245,7 +245,7 @@ function SkillsCard() {
 function RoleCard() {
   return (
     <RevealCard
-      className="col-span-2 md:col-span-6 lg:col-span-4 card-role-quote rounded-[20px] flex flex-col justify-between"
+      className="col-span-1 md:col-span-6 lg:col-span-4 card-role-quote rounded-[20px] flex flex-col justify-between"
       style={{
         background: "#fffbe8",
         color: "#0b0a1a",
